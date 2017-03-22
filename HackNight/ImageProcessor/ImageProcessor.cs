7a018193d62c0974d063ae62f3a65194cb2662a2
brettberliner <bberliner@gmail.com>
2017-03-22 09:13:06 -0400

    updates to pull functions from Azure into project

---
+++ HackNight/ImageProcessor/ImageProcessor.cs
@@ -0,0 +1,33 @@
+﻿#r "System.Drawing"
+using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+
+namespace HackNight.ImageProcessor
+{
+    public class ImageProcessor
+    {
+
+        public static void Run(string myQueueItem, TraceWriter log, out string outputBlob)
+        {
+            {
+                log.Info($"C# Queue trigger function processed: {myQueueItem}");
+                using (WebClient webClient = new WebClient())
+                {
+                    byte[] data = webClient.DownloadData(myQueueItem);
+
+                    using (MemoryStream mem = new MemoryStream(data))
+                    {
+                        outputBlob = Convert.ToBase64String(mem.ToArray());
+
+                        using (var image = System.Drawing.Image.FromStream(mem))
+                        {
+                            log.Info(image.Height.ToString());
+                        }
+                    }
+
+                }
+            }
+}
---
+++ HackNight/MessageGenerator/index.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+
+    if (myTimer.isPastDue) {
+        context.log('JavaScript is running late!');
+    }
+
+    var toBeQed = myQueueItem();
+    toBeQed.time = timeStamp;
+    context.bindings.myQueueItem = toBeQed;
+
+    context.log('JavaScript timer trigger function ran!', timeStamp);
+
+    context.done();
+};
+
+function myQueueItem() {
+    return {
+        msg: "http://urlnextdoor.com/ai/design-layout/images/sample-png.png",
+        time: "time goes here"
+    }
+}
